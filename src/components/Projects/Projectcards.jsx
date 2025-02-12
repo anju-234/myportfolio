c628a01e6ea4aa0e9273057649cde05e09a38b58
anju-234 <anjum23561@gmail.com>
2025-02-12 14:07:18 +0900

    projectcard changes

--- src/components/Projects/Projectcards.jsx
+++ src/components/Projects/Projectcards.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./ProjectCard.css";
+import "./Projectcard.css";
 import { Card, Button } from "react-bootstrap";
 
 function ProjectCard({ project }) {
